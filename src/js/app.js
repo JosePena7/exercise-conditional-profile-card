3efa776fa8647c22eeee7f358d1df55b8412eb2e
JosePena7 <Josesworkday23@icloud.com>
2025-11-08 18:21:59 -0600

    Conditional-card

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { name } from "file-loader";
 import "../style/index.css";
 
 /**
@@ -33,10 +34,19 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name ? variables.name : "Jose"} ${
+    variables.lastName ? variables.lastName : "Peña"
+  }</h1>
+          <h2>${variables.role ? variables.role : `landscaper`}</h2>
+          <h3>${variables.city ? variables.city : `Los Sauces`} ${
+    variables.country ? variables.country : `Mexico`
+  }</h3>
+
+            <ul class="position-${
+              variables.socialMediaPosition === "position-left"
+                ? "left"
+                : "right"
+            }">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
